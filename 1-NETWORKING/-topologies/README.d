8f286dfc2e5dcadc9bd9e2278a7bfe923f06e91b
Zainab Ijaz <zainabijaz6633@gmail.com>
2026-07-06 20:45:57 -0700

    Create README.d

---
+++ 1-NETWORKING/-topologies/README.d
@@ -0,0 +1,22 @@
+# 🖧 Network Topologies
+
+## Overview
+
+This folder contains different network topology designs, diagrams, advantages, disadvantages, and real-world applications.
+
+## Topologies
+
+- Bus
+- Star
+- Ring
+- Mesh
+- Tree
+- Hybrid
+
+## Learning Objective
+
+To understand how different network topologies are designed and deployed.
+
+---
+
+**Author:** Zainab Ijaz
